b10f14bac89ff2dfbdb941c5085484223c96a673
Firestar311 <firestar311mc@gmail.com>
2018-05-22 01:13:07 -0500

    [Untested] Implement commands for warps

--- src/java/net/firecraftmc/core/managers/WarpManager.java
+++ src/java/net/firecraftmc/core/managers/WarpManager.java
@@ -1,6 +1,8 @@
 package net.firecraftmc.core.managers;
 
 import net.firecraftmc.core.FirecraftCore;
+import net.firecraftmc.shared.classes.FirecraftPlayer;
+import net.firecraftmc.shared.classes.Messages;
 import net.firecraftmc.shared.classes.Warp;
 import net.firecraftmc.shared.enums.Rank;
 import org.bukkit.Bukkit;
@@ -12,6 +14,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,6 +25,7 @@ public class WarpManager implements CommandExecutor {
 
     private FirecraftCore plugin;
     private List<Warp> warps = new ArrayList<>();
+    private static final String prefix = "&d&l[Warps] ";
 
     private File file;
     private FileConfiguration config;
@@ -65,6 +69,87 @@ public class WarpManager implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Messages.onlyPlayers);
+            return true;
+        }
+
+        FirecraftPlayer player = plugin.getPlayerManager().getPlayer(((Player) sender).getUniqueId());
+
+        if (!(args.length > 0)) {
+            player.sendMessage(prefix + Messages.notEnoughArgs);
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("setwarp")) {
+            if (player.getMainRank().isEqualToOrHigher(Rank.ADMIN)) {
+                Warp warp;
+                if (args.length == 2) {
+                    Rank rank;
+                    try {
+                        rank = Rank.valueOf(args[1]);
+                    } catch (Exception e) {
+                        player.sendMessage(prefix + Messages.invalidRank);
+                        return true;
+                    }
+                    warp = new Warp(args[0], player.getLocation(), rank);
+                } else {
+                    warp = new Warp(args[0], player.getLocation());
+                }
+                this.warps.remove(warp);
+                this.warps.add(warp);
+                player.sendMessage(prefix + Messages.setWarp(warp));
+            } else {
+                player.sendMessage(prefix + Messages.noPermission);
+                return true;
+            }
+        } else if (cmd.getName().equalsIgnoreCase("delwarp")) {
+            if (player.getMainRank().isEqualToOrHigher(Rank.ADMIN)) {
+                Warp warp = null;
+                for (Warp w : warps) {
+                    if (w.getName().equalsIgnoreCase(args[0])) {
+                        warp = w;
+                    }
+                }
+
+                if (warp == null) {
+                    player.sendMessage(prefix + Messages.warpDoesNotExist);
+                    return true;
+                }
+
+                if (warp.getMinimumRank().isHigher(player.getMainRank())) {
+                    player.sendMessage(prefix + Messages.noPermission);
+                    return true;
+                }
+
+                this.warps.remove(warp);
+                player.sendMessage(prefix + Messages.delWarp(warp.getName()));
+            } else {
+                player.sendMessage(prefix + Messages.noPermission);
+                return true;
+            }
+        } else if (cmd.getName().equalsIgnoreCase("warp")) {
+            Warp warp = null;
+            for (Warp w : warps) {
+                if (w.getName().equalsIgnoreCase(args[0])) {
+                    warp = w;
+                }
+            }
+
+            if (warp == null) {
+                player.sendMessage(prefix + Messages.warpDoesNotExist);
+                return true;
+            }
+
+            if (warp.getMinimumRank().isHigher(player.getMainRank())) {
+                player.sendMessage(prefix + Messages.noPermission);
+                return true;
+            }
+
+            player.teleport(warp.getLocation());
+            player.sendMessage(prefix + Messages.warpTeleport(warp.getName()));
+        }
+
         return true;
     }
 }
