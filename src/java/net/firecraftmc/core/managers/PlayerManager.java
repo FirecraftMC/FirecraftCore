aceed05d063cd32165d4ea7bf057721c85483fed
Firestar311 <firestar311mc@gmail.com>
2018-06-11 14:13:44 -0500

    Made a method in the database class to update a certain column for a player. This will be useful for additional subcommands for the players command

--- src/java/net/firecraftmc/core/managers/PlayerManager.java
+++ src/java/net/firecraftmc/core/managers/PlayerManager.java
@@ -442,7 +442,7 @@ public class PlayerManager implements IPlayerManager, Listener {
                             return true;
                         }
 
-                        plugin.getFCDatabase().updateSQL("UPDATE `playerdata` SET `mainrank` = '" + rank.toString() + "' WHERE `uniqueid`='{uuid}';".replace("{uuid}", target.getUniqueId().toString()));
+                        plugin.getFCDatabase().updateDataColumn(player.getUniqueId(), "mainrank", rank.toString());
                         player.sendMessage(Messages.setMainRank(target.getName(), rank));
                         FPacketRankUpdate rankUpdate = new FPacketRankUpdate(plugin.getFirecraftServer(), player.getUniqueId(), target.getUniqueId());
                         plugin.getSocket().sendPacket(rankUpdate);
